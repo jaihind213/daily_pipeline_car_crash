98dff2c6c596c26ce049d2f4be00f1f6daa7e51e
vishnu chanderraju <vishnu.ch@cuezen.com>
2025-07-13 14:36:41 +0530

    trying infra as code

--- infra-as-code/__main__.py
+++ infra-as-code/__main__.py
@@ -1,11 +1,7 @@
 import logging
-import os
 
 import pulumi
 import pulumi_digitalocean as do
-import traceback
-#from pulumi import InvokeError
-
 
 region = "sgp1"
 project_name = "first-project"
@@ -13,15 +9,19 @@ cluster_name = "k8s-1-33-1-do-1-sgp1-1752378431833"
 version = "1.33.1-do.1"
 pool_name = "pool-l7g14a0wb"
 tags = ["k8s", "testing", project_name]
-node_size= "s-2vcpu-4gb"
-num_nodes=3
+node_size = "s-2vcpu-4gb"
+num_nodes = 3
 
 
-def create_k8s_cluster(cluster_name, region,version,tags, pool_name, node_size, num_nodes, project_name):
-    logging.info(f"creating k8s cluster with name: {cluster_name}, region: {region}, project_name:{project_name}")
+def create_k8s_cluster(
+    name, region, version, tags, pool_name, node_size, num_nodes, project_name
+):
+    logging.info(
+        f"creating k8s cluster with name: {name}, region: {region}, project_name:{project_name}"  # noqa: E501
+    )
     cluster = do.KubernetesCluster(
-        resource_name=cluster_name,
-        name=cluster_name,
+        resource_name=name,
+        name=name,
         region=region,
         version=version,
         tags=tags,
@@ -29,29 +29,13 @@ def create_k8s_cluster(cluster_name, region,version,tags, pool_name, node_size, 
             name=pool_name,
             size=node_size,
             node_count=num_nodes,
-        )
+        ),
     )
-    pulumi.export("k8s_cluster_id", cluster.id)
-    pulumi.export("k8s_cluster_urn", cluster.cluster_urn)
+    return cluster.id, cluster.cluster_urn
 
 
-try:
-    existing_cluster = do.get_kubernetes_cluster(name=cluster_name)
-    resource_name=cluster_name
-    #existing_cluster = do.get(resource_name, os.environ.get('K8S_CLUSTER_ID', ''))
-    create_k8s_cluster(cluster_name, region, version, tags, pool_name, node_size, num_nodes, project_name)
-    #pulumi.export("k8s_cluster_id", existing_cluster.id)
-except Exception as e:
-    if "Unable to find cluster with name" in str(e):
-        pulumi.log.warn(f"**** Cluster '{cluster_name}' does not exist — will create it.")
-        create_k8s_cluster(cluster_name, region, version, tags, pool_name, node_size, num_nodes, project_name)
-    else:
-        traceback.print_exc()
-#
-# try:
-#     cluster = do.KubernetesCluster.get("existing-cluster", cluster_name, opts=pulumi.ResourceOptions())
-#     pulumi.export("k8s_cluster_id", cluster.id)
-# except Exception as e:
-#     pulumi.log.warn(f"Cluster '{cluster_name}' details could not be fetched")
-#     traceback.print_exc()
-#     #create_k8s_cluster()
+cluster_id, cluster_urn = create_k8s_cluster(
+    cluster_name, region, version, tags, pool_name, node_size, num_nodes, project_name
+)
+pulumi.export("k8s_cluster_id", cluster_id)
+pulumi.export("k8s_cluster_urn", cluster_urn)
