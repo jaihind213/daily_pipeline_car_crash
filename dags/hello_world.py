9a079514e2b73999467b13bb5bea9e9f060ffb63
vishnu chanderraju <vishnu.ch@cuezen.com>
2025-07-19 08:29:45 +0530

    hello world dag with date param

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -1,21 +1,28 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
 from datetime import datetime
 
+from airflow import DAG
+from airflow.models import Param
+from airflow.operators.python import PythonOperator
 
-def say_hello():
+
+def say_hello(**context):
     import time
+
     print("before sleep")
     time.sleep(60)
-    print("Hello, world!")
+    date = context["params"]["date"]
+    print("Hello, world!", date)
 
 
 with DAG(
     dag_id="hello_world",
-    start_date=datetime(2024, 1, 1),
+    start_date=datetime.now(),
     schedule_interval="@daily",  # Run once per day
-    catchup=False,               # Don't backfill missed runs
+    catchup=False,  # Don't backfill missed runs
     tags=["example"],
+    params={
+        "date": Param("2024-04-20", type="string"),
+    },
 ) as dag:
 
     hello_task = PythonOperator(
