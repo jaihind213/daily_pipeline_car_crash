1f25809f8b440123855dc1b0f211bb3452bfe4de
vishnu chanderraju <vishnu.ch@cuezen.com>
2025-07-19 07:43:32 +0530

    enable webserver basic auth

--- trigger_dag.sh
+++ trigger_dag.sh
@@ -7,7 +7,7 @@ AIRFLOW_HOST=$4
 AIRFLOW_PORT=8080
 
 EPOCH_TIME=$(date +%s)
-RUN_ID="angiogram_runid_${EPOCH_TIME}"
+RUN_ID="dag_runid_${EPOCH_TIME}"
 
 curl -X PATCH  "http://${AIRFLOW_HOST}:8080/api/v1/dags/$DAG_NAME" \
 -H "Content-Type: application/json" \
@@ -26,5 +26,5 @@ curl -X POST "http://${AIRFLOW_HOST}:8080/api/v1/dags/${DAG_NAME}/dagRuns" \
   \"note\": \"string\"
 }"
 
-touch /tmp/angiogram_dag_run_id
-echo $RUN_ID >> /tmp/angiogram_dag_run_id
+touch /tmp/dag_run_id
+echo $RUN_ID >> /tmp/dag_run_id
