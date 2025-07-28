718435ed4cd7c84fb4e41139c73a6aafa03f76a2
vishnu chanderraju <vishnu.ch@cuezen.com>
2025-07-28 12:03:23 +0530

    python path for dag

--- dags/dag_util.py
+++ dags/dag_util.py
@@ -65,6 +65,10 @@ def create_py_spark_operator_app_file(
                         "name": "SPARK_CLASSPATH",
                         "value": "/opt/spark/conf:/opt/spark_jars/*:/opt/spark/jars/*",
                     },
+                    {
+                        "name": "PYTHONPATH",
+                        "value": "/opt/daily_pipeline_car_crash"
+                    },
                 ],
                 "envFrom": [{"secretRef": {"name": secret_holding_env_vars}}],
                 "volumeMounts": [
@@ -92,6 +96,10 @@ def create_py_spark_operator_app_file(
                     {
                         "name": "SPARK_CLASSPATH",
                         "value": "/opt/spark/conf:/opt/spark_jars/*:/opt/spark/jars/*",
+                    },
+                    {
+                        "name": "PYTHONPATH",
+                        "value": "/opt/daily_pipeline_car_crash"
                     },
                 ],
                 "envFrom": [{"secretRef": {"name": secret_holding_env_vars}}],
