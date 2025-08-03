968aafd92803af15db9fab05cf7e0507e0b573d5
vishnu chanderraju <vishnu.ch@cuezen.com>
2025-08-03 18:53:47 +0530

    github branch to templating for dags

---
+++ infra-as-code/export_app_secrets_to_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#we have a single secret in github_secrets in the form of a json.
+#ex:
+#{ "SOCRATA_TOKEN": "user1", "S3_ACCESS_KEY": "pass1", "S3_SECRET_KEY": "abc123", "ICEBERG_PATH": "", "RAW_DATA_PATH": "" }
+# this has been done to avoid multiple exports in the github workflow file.
+JSONFILE=$1
+for key in $(jq -r 'keys[]' $JSONFILE); do
+  value=$(jq -r --arg k "$key" '.[$k]'  $JSONFILE)
+  echo "$key=$value" >> $GITHUB_ENV
+  export $key=$value
+done
