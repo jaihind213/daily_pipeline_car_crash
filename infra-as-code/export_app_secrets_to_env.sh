536334608592ccd37adb01e54973f312bba6784b
vishnu chanderraju <vishnu.ch@cuezen.com>
2025-08-03 20:57:29 +0530

    changing secret name for app. sourcing the secrets env file

--- infra-as-code/export_app_secrets_to_env.sh
+++ infra-as-code/export_app_secrets_to_env.sh
@@ -5,8 +5,10 @@
 #{ "SOCRATA_TOKEN": "user1", "S3_ACCESS_KEY": "pass1", "S3_SECRET_KEY": "abc123", "ICEBERG_PATH": "", "RAW_DATA_PATH": "" }
 # this has been done to avoid multiple exports in the github workflow file.
 JSONFILE=$1
+KEY_FILE=$2
 for key in $(jq -r 'keys[]' $JSONFILE); do
   value=$(jq -r --arg k "$key" '.[$k]'  $JSONFILE)
   echo "$key=$value" >> $GITHUB_ENV
-  export $key=$value
+  echo "$key=$value" >> $KEY_FILE
+  chmod 400 $KEY_FILE
 done
