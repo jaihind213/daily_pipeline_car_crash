28e62dff0be20ead4cadba2a03039ede805b4944
vishnu chanderraju <vishnu.ch@cuezen.com>
2025-08-04 06:48:05 +0530

    changing secret name for app. sourcing the secrets env file.

--- infra-as-code/export_app_secrets_to_env.sh
+++ infra-as-code/export_app_secrets_to_env.sh
@@ -6,9 +6,10 @@
 # this has been done to avoid multiple exports in the github workflow file.
 JSONFILE=$1
 KEY_FILE=$2
+touch $KEY_FILE
+chmod 400 $KEY_FILE
 for key in $(jq -r 'keys[]' $JSONFILE); do
   value=$(jq -r --arg k "$key" '.[$k]'  $JSONFILE)
   echo "$key=$value" >> $GITHUB_ENV
   echo "$key=$value" >> $KEY_FILE
-  chmod 400 $KEY_FILE
 done
